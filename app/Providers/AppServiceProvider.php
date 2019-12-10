25d37fcdc525ab6d356749c8add1dc9daf4ef335
zendzo <zendzzo@gmail.com>
2019-12-10 16:11:06 +0700

    delete sample file of adminbsb

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
-use App\Role;
+use Illuminate\Support\Facades\Schema;
 use App\MenuCategory;
+use App\Role;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -16,6 +17,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
+        
         if (! $this->app->runningInConsole()) {
 
             view()->share('roles', Role::all());
